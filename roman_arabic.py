ca18c70869ce6be90eb53be4e877005189ca1400
Mirko Leccese <leccese_mirko@yahoo.it>
2023-02-19 11:33:21 +0100

    add a list compr for conversion

--- roman_arabic.py
+++ roman_arabic.py
@@ -12,17 +12,23 @@ dict_roman = {
 
 test_roman = 'CMIV'
 
-value = dict_roman[test_roman[-1]]
+#value = dict_roman[test_roman[-1]]
 #print(value)
 
-arabic = value
-for i in reversed(test_roman[:-1]):
-    value_next = dict_roman[i]
-    if value_next < value:
-        arabic -= value_next
-    else:
-        arabic += value_next
-    
-    value = value_next
+#arabic = value
+#for i in reversed(test_roman[:-1]):
+#    value_next = dict_roman[i]
+#    if value_next < value:
+#        arabic -= value_next
+#    else:
+#        arabic += value_next
+#    
+#    value = value_next
 
-print(arabic)
+#print(arabic)
+
+
+values = [ dict_roman[roman_char] if i == 0 or dict_roman[test_roman[i-1]] <= dict_roman[roman_char]  
+    else -dict_roman[roman_char] for i, roman_char in enumerate(reversed(test_roman))]
+
+print(values)
